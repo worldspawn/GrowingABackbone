1e2d1b9f179ea6176d5f9822abcc4768d72edea6
Sam Critchley <critchley.sj@gmail.com>
2011-08-03 00:38:42 +1000

    got an add view going. half works

--- GrowingABackbone/Controllers/GameController.cs
+++ GrowingABackbone/Controllers/GameController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace GrowingABackbone.Controllers
 {
@@ -15,41 +16,71 @@ namespace GrowingABackbone.Controllers
         }
 
         [HttpGet]
-        public ActionResult Data(Guid? gameId)
+        [ActionName("Data")]
+        public ActionResult Get(Guid? gameId)
         {
             if (gameId == null)
                 return Json(games, JsonRequestBehavior.AllowGet);
 
-            return Json(games.FirstOrDefault(g => g.Id == gameId.Value), JsonRequestBehavior.AllowGet);
+            return Json(games.FirstOrDefault(g => g.id == gameId.Value), JsonRequestBehavior.AllowGet);
         }
-
-        [HttpPost]
-        public ActionResult Data(Game game)
+        
+        [HttpPut]
+        [ActionName("Data")]
+        public ActionResult Put(Game game)
         {
-            var dsGame = games.First(p => p.Id == game.Id);
+            var dsGame = games.First(p => p.id == game.id);
             games[games.ToList().IndexOf(dsGame)] = game;
             return new EmptyResult();
         }
 
-        static readonly Game[] games = new[]
+        [HttpDelete]
+        [ActionName("Data")]
+        public ActionResult Delete(Game game)
         {
-            new Game{ Id = Guid.NewGuid(), Name="Portal" },
-            new Game{ Id = Guid.NewGuid(), Name="Portal 2" },
-            new Game{ Id = Guid.NewGuid(), Name="Star Wars: Knights of the Old Republic" },
-            new Game{ Id = Guid.NewGuid(), Name="Battlefield: Bad Company 2" },
-            new Game{ Id = Guid.NewGuid(), Name="Team Fortress" },
-            new Game{ Id = Guid.NewGuid(), Name="Populous" },
-            new Game{ Id = Guid.NewGuid(), Name="Flashback" },
-            new Game{ Id = Guid.NewGuid(), Name="Another World" },
+            var dsGame = games.First(p => p.id == game.id);
+            games.Remove(dsGame);
+
+            return new EmptyResult();            
+        }
+
+        [HttpPost]
+        [ActionName("Data")]
+        public ActionResult Post([Bind(Exclude = "Id,id")]Game game)
+        {
+            games.Add(game);
+            game.id = Guid.NewGuid();
+
+            return Json(game);
+        }
+
+        static readonly IList<Game> games = new List<Game>
+        {
+            new Game{ id = Guid.NewGuid(), Name="Portal" },
+            new Game{ id = Guid.NewGuid(), Name="Portal 2" },
+            new Game{ id = Guid.NewGuid(), Name="Star Wars: Knights of the Old Republic" },
+            new Game{ id = Guid.NewGuid(), Name="Battlefield: Bad Company 2" },
+            new Game{ id = Guid.NewGuid(), Name="Team Fortress" },
+            new Game{ id = Guid.NewGuid(), Name="Populous" },
+            new Game{ id = Guid.NewGuid(), Name="Flashback" },
+            new Game{ id = Guid.NewGuid(), Name="Another World" },
         };
     }
-
-    public class Game
+    
+    public interface IBackboneModel
     {
-        public Guid Id { get; set; }
+        Guid? id { get; set; }
+    }
+    
+    [Bind(Exclude="Id")]
+    public class BackboneModel : IBackboneModel
+    {
+        public Guid? id { get; set; }
+    }
+
+    public class Game : BackboneModel
+    {
         public string Name { get; set; }
         public bool IsWatched { get; set; }
     }
-
-    
 }
